75a168781922158dbb9f76789763f4d6b32cc6e2
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2022-04-19 23:10:12 -0400

    Write to database text file

--- main.py
+++ main.py
@@ -8,9 +8,17 @@ def home():
 
 @app.route("/api/submit", methods=['POST'])
 def api():
-    api = request.json
-    print(request.get_json()["test"])
-    return "OK"
+    try:
+        url = request.get_json()["url"]
+        with open("bot/database.txt", "a") as myfile: # database is stored under the bot
+            myfile.write(url + "\n")
+        return "OK"
+    except:
+        return "Bad Request"
+
+@app.route("/api/db")
+def api():
+    return "Working on this"
 
 if __name__ == "__main__":
     app.run(port=4001,host='0.0.0.0')
