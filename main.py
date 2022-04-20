de65b7e3275858cd24cb5f7f18a0db2a7e38ae9a
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2022-04-19 23:14:02 -0400

    more verbose

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ def home():
 def api():
     try:
         url = request.get_json()["url"]
-        with open("bot/database.txt", "a") as myfile: # database is stored under the bot
-            myfile.write(url + "\n")
-        return "OK"
+        try:
+            with open("bot/database.txt", "a") as myfile: # database is stored under the bot
+                myfile.write(url + "\n")
+            return "OK"
+        except:
+            return "Bad Request (unable to open txt/db file)"
     except:
         return "Bad Request"
 
