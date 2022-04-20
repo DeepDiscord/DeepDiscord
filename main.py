f012ed9d33093e7e095358fb749d36a267457aed
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2022-04-19 23:18:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request, jsonify
+import sys
 
 app = Flask(__name__)
+
+# For testing, this is a fake database of valid auth tokens
+# Note to self, take this out before production
+
+keys = ["abc123", "000webhostSUCKS"]
 
 @app.route("/")
 def home():
@@ -10,14 +16,17 @@ def home():
 def api():
     try:
         url = request.get_json()["url"]
+        auth = request.get_json()["token"]
+        if auth not in keys:
+            return "Bad Request, invalid token"
         try:
             with open("bot/database.txt", "a") as myfile: # database is stored under the bot
                 myfile.write(url + "\n")
             return "OK"
         except:
-            return "Bad Request (unable to open txt/db file)"
+            return "Internal Server Error (unable to open txt/db file)"
     except:
-        return "Bad Request"
+        return "Bad Request, check your post data"
 
 @app.route("/api/db")
 def api_db():
