5a7478a7a03355a0e03640aded6ca95605b7f8c4
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2022-04-19 23:03:21 -0400

    string

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def home():
 @app.route("/api/submit", methods=['POST'])
 def api():
     api = request.json
-    print(request.get_json()["test"])
+    print(str(request.get_json()))
     return "OK"
 
 if __name__ == "__main__":
