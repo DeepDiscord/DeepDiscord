879053e8382460cade4d8f5d102d87d093cefae8
RiversideRocks <wilest7@gmail.com>
2022-04-22 22:24:31 -0400

    Make javascript work when the error is fixed on Stackoverflow

--- static/ajax.js
+++ static/ajax.js
@@ -1,9 +1,10 @@
-function search(invite){
+function search(invite){ // asumming invite ID is the same one as the javascript element ID
     $.get( "/api/invite/" + invite, function( data ) {
         if(data.error != "true"){
-            alert(data.name)
+            document.getElementById(invite).innerHTML = document.getElementById(invite).innerHTML + " - Server Name: " + data.name
+            console.log("Got server name via Discord's API: " + data.name)
         }else{
-            alert("Invalid/Expired invite")
+            console.warn("Invalid/Expired invite (ID: " + invite + ")")
         }
     });
 }
