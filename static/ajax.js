e221c238c86212e0c87d021d4424259d341fe178
RiversideRocks <wilest7@gmail.com>
2022-04-23 08:16:30 -0400

    Better Search Results

--- static/ajax.js
+++ static/ajax.js
@@ -4,6 +4,7 @@ function search(invite){ // asumming invite ID is the same one as the javascript
             document.getElementById(invite).innerHTML = document.getElementById(invite).innerHTML + " - Server Name: " + data.name
             console.log("Got server name via Discord's API: " + data.name)
         }else{
+            document.getElementById(invite).innerHTML = document.getElementById(invite).innerHTML + " - <strong color='red'>Invalid/Expired Invite</strong>"
             console.warn("Invalid/Expired invite (ID: " + invite + ")")
         }
     });
